0b5185b3856079e7c60295f5df3a3698aee48808
Vinicius José Fritzen <www.vini842000@hotmail.com>
2024-07-24 11:39:02 -0300

    Demonstracao copia profunda

--- alunos.cpp
+++ alunos.cpp
@@ -11,5 +11,21 @@ int main(void)
   std::cout << *a1 << std::endl;
   std::cout << *p1 << std::endl;
 
+  Pessoa p;
+  p.set_nome("Primeiro Teste");
+  std::cout << "p antes: " << p << std::endl;
+  Pessoa p2 = p;
+  p.set_nome("Teste Dois");
+  std::cout << "p depois: " << p << std::endl;
+  std::cout << "p2: " << p2 << std::endl;
+
+  Pessoa p3;
+  p3 = p;
+  p.set_nome("Teste Final");
+
+  std::cout << "p final: " << p << std::endl;
+  std::cout << "p2: " << p2 << std::endl;
+  std::cout << "p3: " << p3 << std::endl;
+
   return 0;
 }
